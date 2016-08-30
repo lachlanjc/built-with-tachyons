e7de8e80b359a4a49ce8ebb6edbe9faadca56380
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-08-30 11:35:47 -0400

    enlarge cards on mobile

--- src/CardLink.js
+++ src/CardLink.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const CardLink = (props) => (
-  <a {...props} target='_blank' className='no-underline pa2 w-40 w-25-ns mw-10 mw-20-ns' />
+  <a {...props} target='_blank' className='no-underline border-box pa2 w-50 min-50-sm w-25-ns mw-10 mw-20-ns' />
 )
 
 export default CardLink
